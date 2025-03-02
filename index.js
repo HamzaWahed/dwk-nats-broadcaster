d322ff323ccc843532ac19fddcb423e5dbf43fa8
hamza wahed <hzd454@gmail.com>
2025-03-02 15:51:12 -0400

    update broadcaster with async functions

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
+require("dotenv").config();
 const { connect, JSONCodec } = require("nats");
+let nc;
 
-nc = await connect({ servers: process.env.NATS_URL });
-jc = JSONCodec();
+async function initNats() {
+  try {
+    nc = await connect({ servers: process.env.NATS_URL });
+    console.log("NATS connected successfully");
+  } catch (err) {
+    console.error("Failed to connect to NATS:", error);
+    process.exit(1); // Exit if we can't connect to NATS
+  }
+}
 
+initNats();
+
+const jc = JSONCodec();
 const sub = nc.subscribe("todo_status");
 
-(async () => {
-  for await (const m of sub) {
-    console.log(`[${sub.getProcessed()}]: ${jc.decode(m.data)}`);
-  }
+async function logMessages() {
+  try {
+    for await (const m of sub) {
+      console.log(`[${sub.getProcessed()}]: ${jc.decode(m.data)}`);
+    }
 
-  console.log("subscription closed");
-})();
+    console.log("subscription closed");
+  } catch (err) {
+    console.error("Error connecting to NATS:", err);
+  }
+}
+
+logMessages();
